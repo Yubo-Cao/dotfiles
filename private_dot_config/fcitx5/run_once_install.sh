ae4c0e916c5c1be4817b7124a13d5727ffdeb788
Yubo Cao <cao2006721@gmail.com>
2023-04-05 16:40:42 -0400

    fix: fix the installation script of fcitx5

--- private_dot_config/fcitx5/run_once_install.sh
+++ private_dot_config/fcitx5/run_once_install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 source "$CHEZMOI_SOURCE_DIR/common.sh"
 
-install "Fcitx5" \ 
+install "Fcitx5" \
     fcitx5 fcitx5-configtool fcitx5-gtk fcitx5-qt \
     fcitx5-chinese-addons fcitx5-lua \
     fcitx5-nord

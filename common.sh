f0fbf0f0c4607bf2735bae4a77739995227f2947
Yubo Cao <cao2006721@gmail.com>
2023-04-05 16:15:29 -0400

    feat: setup waybar

---
+++ common.sh
@@ -0,0 +1,105 @@
+#!/usr/bin/bash
+
+# logging
+error() {
+  echo -e "\e[31m[Error] $1\e[0m"
+}
+
+warn() {
+  echo -e "\e[33m[Warning] $1\e[0m"
+}
+
+info() {
+  echo -e "\e[32m[Info] $1\e[0m"
+}
+
+
+# bootstrap aur
+bootstrap_aur() {
+  sudo pacman -S --needed --noconfirm base-devel git > /dev/null 2>&1 \
+      && info "AUR bootsrapped successfully" \
+      || error "Failed to bootsrap aur"
+}
+
+# progress bar
+progress_bar() {
+  local percentage=$1
+  local message=$2
+  local bar_type=$3
+
+  local width=$(( $(tput cols) / 2 ))
+  width=$(( width > 80 ? width : 80 ))
+  width=$(( width - ${#message}))
+  if [[ -n "$message" ]]; then width=$(( width - 3 )); fi
+  (( width = width < 0 ? 0 : width ))
+
+  local progress=$((percentage * width / 100))
+  local remaining=$((width - progress))
+  local progress_bar=$(printf "[%s%s]" $(printf "#%.0s" $(seq 1 $progress)) "$(printf ' %.0s' $(seq 1 $remaining))")
+  local final_message="${message:+[${message}] }${progress_bar} ${percentage}%"
+
+  case "$bar_type" in
+    info) printf "\r\e[32m%s\e[0m" "$final_message" ;;
+    warn) printf "\r\e[33m%s\e[0m" "$final_message" ;;
+    error) printf "\r\e[31m%s\e[0m" "$final_message" ;;
+    *) printf "\r%s" "$final_message" ;;
+  esac
+}
+
+# yay
+install_yay() {
+  if yay --version &> /dev/null; then
+    warn "yay is already installed"
+  else
+    bootstrap_aur
+    temp=$(mktemp -d)
+    git clone https://aur.archlinux.org/yay-bin.git $temp/yay-bin
+    cd $temp/yay-bin
+    makepkg -si --noconfirm
+    cd ~
+    rm -rf $temp
+  fi
+}
+
+# aur
+install() {
+  if ! command -v yay &> /dev/null; then
+    info "yay is not installed. Installing yay..."
+    install_yay
+  fi
+
+  local title=$1
+  shift
+  local packages=("$@")
+  local total_packages=${#packages[@]}
+  local progress=0
+  local success=()
+  local failure=()
+
+  info "Installing $title..."
+
+  for package in "${packages[@]}"; do
+    ((progress++))
+    local percentage=$((progress * 100 / total_packages))
+    progress_bar "$percentage" "$package"
+    if yay -S --noconfirm --needed "$package" > /dev/null 2>&1; then
+      progress_bar "$percentage" "$package" "info"
+      sleep 0.05
+      success+=("✅ $package")
+    else
+      progress_bar "$percentage" "$package" "error"
+      sleep 0.05
+      failure+=("❌ $package")
+    fi
+  done
+
+  echo
+
+  if [ ${#success[@]} -gt 0 ]; then
+    printf '%s\n' "${success[@]}"
+  fi
+
+  if [ ${#failure[@]} -gt 0 ]; then
+    printf '%s\n' "${failure[@]}"
+  fi
+}
---
+++ private_dot_config/waybar/run_once_install.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/bash
+source "$CHEZMOI_SOURCE_DIR/common.sh"
+
+install Waybar \
+    waybar ttf-font-awesome light

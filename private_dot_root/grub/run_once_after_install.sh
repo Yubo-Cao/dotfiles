a0c865b145bd8b484381722cfc17c80efe0c76c2
Yubo Cao <cao2006721@gmail.com>
2023-04-06 09:16:52 -0400

    feat: add matter grub theme

--- private_dot_root/grub/run_once_after_install.sh
+++ private_dot_root/grub/run_once_after_install.sh
@@ -2,44 +2,15 @@
 
 source "$CHEZMOI_SOURCE_DIR/common.sh"
 
-set -euo pipefail
+info "Install matter dependencies"
+install "Matter" \
+    inkscape \
+    grub-customizer
 
-name="minimal"
-
-install_theme() {
-    info "Installing theme..."
-    if sudo mkdir -p "/boot/grub/themes/$name"; then
-        info "Created /boot/grub/themes/$name"
-    else
-        error "Failed to create /boot/grub/themes/$name"
-    fi
-
-    if sudo cp -r "$HOME/.root/grub/$name" "/boot/grub/themes/"; then
-        info "Copied theme to /boot/grub/themes/$name"
-    else
-        error "Failed to copy theme to /boot/grub/themes/$name"
-    fi
-}
-
-modify_grub() {
-    info "Backing up /etc/default/grub to /etc/default/grub.bak"
-    sudo cp /etc/default/grub{,.bak}
-
-    info "Modifying /etc/default/grub..."
-    modify_kv_config "GRUB_GFXMODE" "3072x1920x32,auto" "/etc/default/grub"
-    modify_kv_config "GRUB_THEME" "/boot/grub/themes/$name/theme.txt" "/etc/default/grub"
-
-    info "Saving grub config to /boot/grub/grub.cfg..."
-    if sudo grub-mkconfig -o /boot/grub/grub.cfg >/dev/null 2>&1; then
-        info "Saved grub config to /boot/grub/grub.cfg"
-    else
-        error "Failed to save grub config to /boot/grub/grub.cfg"
-    fi
-}
-
-if ! command -v grub-mkconfig &>/dev/null; then
-    info "grub is not installed. Skipping..."
-    exit 0
-fi
-install_theme
-modify_grub
+info "Install matter"
+sudo python "$HOME/.root/grub/matter/matter.py" \
+        --fontname 'Inter Regular' \
+        --fontfile /usr/share/fonts/inter/Inter-Regular.ttf \
+        --icons arch _ _ _ _ _ microsoft-windows folder \
+        --highlight 'bfdbfe' --foreground '1e293b' --background 'f8fafc'
+        # blue-200 slate-800 slate-50

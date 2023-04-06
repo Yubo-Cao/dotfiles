d18de222c83b08ba652ab4696e22b2c9381a6a63
Yubo Cao <cao2006721@gmail.com>
2023-04-06 16:52:13 -0400

    feat: add hypr

---
+++ private_dot_config/mako/run_once_after_install.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/bash
+
+source "$CHEZMOI_SOURCE_DIR/common.sh"
+
+install "Mako" \
+    mako
--- private_dot_config/sway/run_once_install.sh
+++ private_dot_config/sway/run_once_install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 source "$CHEZMOI_SOURCE_DIR/common.sh"
 
-install "Display fonts for Sway" \
+install "Display fonts" \
     noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra \
     ttf-firacode-nerd \
     inter-font \
@@ -24,12 +24,12 @@ install "Sway" \
 sudo systemctl enable --now seatd && \
     info "seatd enabled" || \
     error "failed to enable seatd"
-sudo usermod -aG seat $(whoami) \
+sudo usermod -aG seat "$(whoami)" \
     && info "Added user to seat group" || \
     error "failed to add user to seat group"
 
 install "Sway utilies" \
-    grim slurp mako \
+    grim slurp \
     wl-clipboard \
     xdg-utils gnome-keyring xorg-xwayland \
     autotiling-rs \

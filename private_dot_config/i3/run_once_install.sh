e8dd0c3dd6728b9606a1336b4ad2eac10068de16
Yubo Cao <cao2006721@gmail.com>
2023-04-08 07:56:41 -0400

    feat: add install script for i3 and polybar

---
+++ private_dot_config/i3/run_once_install.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/bash
+source "$CHEZMOI_SOURCE_DIR/common.sh"
+
+install fonts \
+    noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra \
+    ttf-firacode-nerd \
+    inter-font \
+    ttf-material-design-icons-webfont
+
+install pipewire \
+    pipewire wireplumber pipewire-audio pipewire-jack pipewire-alsa pipewire-pulse
+
+if [ "$(id -u)" -ne 0 ]; then
+    if systemctl --user enable --now pipewire-pulse &&
+        systemctl --user enable --now pipewire; then
+        info "Pipewire enabled"
+    else
+        error "Failed to enable Pipewire"
+    fi
+else
+    warn "Skipping as user is root"
+fi
+
+
+install i3 \
+    i3-gaps-rounded-git \
+    i3lock xss-lock
+
+install "i3 utilies" \
+    xdg-utils gnome-keyring \
+    polkit-dump-agent xorg-xhost \
+    xdg-desktop-portal xdg-desktop-portal-kde \
+    xclip xsel

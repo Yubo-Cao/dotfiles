0e4b6567f53fb81f9cebe7da5f93077fddc6e35b
Yubo Cao <cao2006721@gmail.com>
2023-04-08 08:44:12 -0400

    feat: fix weird display

--- private_dot_config/i3/scale.sh
+++ private_dot_config/i3/scale.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/bash
 xrandr --output eDP-1 --dpi 220
-for (( i=1; i <= 2; i++ ))
-do
-    xrandr --output HDMI-1 --dpi 96 --scale 1x1 --right-of eDP-1
-    sleep 0.05
-    xrandr --output HDMI-1 --dpi 96 --scale 1.5x1.5 --right-of eDP-1
-done
+sleep 0.05
+xrandr --output HDMI-1 --dpi 96 --scale 1x1 --right-of eDP-1
+sleep 0.05
+xrandr --output HDMI-1 --dpi 96 --scale 1.5x1.5 --right-of eDP-1
+sleep 0.05
+nitrogen --restore --force-setter=xinerama

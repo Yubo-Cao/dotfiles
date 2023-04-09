52955995343d90a20276d7ef9f025684020cd05c
Yubo-Cao <cao2006721@gmail.com>
2023-04-09 09:32:48 -0400

    feat: better multi-monitor support through udev drm daemon

--- private_dot_config/i3/script/scale.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/bash
-xrandr --output eDP-1 --dpi 220
-xrandr --output HDMI-1 --dpi 96 --scale 1x1 --right-of eDP-1
-sleep 1
-xrandr --output HDMI-1 --dpi 96 --scale 1.5x1.5 --right-of eDP-1
-sleep 1
-nitrogen --restore --force-setter=xinerama

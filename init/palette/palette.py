fde8e856e654cdc6839ad9cd79a417b54d6a7c6f
Yubo Cao <cao2006721@gmail.com>
2023-04-08 22:35:57 -0400

    feat: add palette script and init file

---
+++ init/palette/palette.py
@@ -0,0 +1,99 @@
+import requests
+from lxml.etree import HTML
+from pathlib import Path
+from tomlkit import dumps, loads
+from logging import getLogger
+
+logger = getLogger(__name__)
+
+
+def scrape_palette():
+    doc = HTML(
+        requests.get(
+            "https://tailwindcss.com/docs/customizing-colors",
+            headers={
+                "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36"
+            },
+        ).text
+    )
+    data = dict(
+        (
+            el.xpath("./div[1]/text()")[0].lower(),
+            dict(
+                (x.xpath("./div[1]/text()")[0], x.xpath("./div[2]/text()")[0])
+                for x in el.xpath(
+                    './div[2]//div[contains(@class, "flex items-center")]/div[2]'
+                )
+            ),
+        )
+        for el in doc.xpath(
+            "//*[@id='content-wrapper']//*[contains(@class, '2xl:contents')]"
+        )
+    )
+    for k, v in data.items():
+        data[k] = {**{"main": v["500"]}, **v}
+    return data
+
+
+def load_config():
+    CONFIG_PATH = Path(__file__).parent / "palette_config.toml"
+    try:
+        config = loads(CONFIG_PATH.read_text(encoding="utf-8"))
+    except FileNotFoundError:
+        logger.error("Config file not found")
+        exit(1)
+    except Exception:
+        logger.error("Invalid config file")
+        exit(1)
+    return config
+
+
+def convert(colors):
+    def hex_to_rgb(hex):
+        hex = hex.lstrip("#")
+        hlen = len(hex)
+        return f'rgb({", ".join(tuple(str(int(hex[i:i+hlen//3], 16)) for i in range(0, hlen, hlen//3)))})'
+
+    def apply_recursive(dct, fn):
+        result = {}
+        for k, v in dct.items():
+            if isinstance(v, dict):
+                result[k] = apply_recursive(v, fn)
+            else:
+                result[k] = fn(v)
+        return result
+
+    table = {
+        "web": colors,
+        "hex": apply_recursive(colors, lambda s: s.lstrip("#")),
+        "rgb": apply_recursive(colors, lambda s: hex_to_rgb(s)),
+    }
+
+    return table
+
+
+def main():
+    cfg = load_config()
+    try:
+        hues = [cfg[i] for i in "primary secondary tertiary dark".split()]
+        [cfg[i] for i in "background title text border".split()]
+    except KeyError:
+        logger.error("Invalid config file")
+        exit(1)
+    data = scrape_palette()
+    for hue in hues:
+        if hue not in data:
+            logger.error(f"Invalid semantic color: {hue}")
+            exit(1)
+    colors = {hue: data[hue] for hue in hues}
+    for semantic in "background title text border".split():
+        colors[semantic] = {
+            hue: data[cfg[hue]][str(cfg[semantic])]
+            for hue in "primary secondary tertiary dark".split()
+        }
+    table = convert({**colors, **data})
+    print(dumps(dict(data=table)))
+
+
+if __name__ == "__main__":
+    main()

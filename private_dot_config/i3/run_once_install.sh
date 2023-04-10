724afcd60462f8e5dec07bfe9e569db15d2741f3
Yubo-Cao <cao2006721@gmail.com>
2023-04-09 20:02:49 -0400

    feat: font to ensure resolve to SC

--- private_dot_config/i3/run_once_install.sh
+++ private_dot_config/i3/run_once_install.sh
@@ -7,7 +7,8 @@ install fonts \
     noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra \
     ttf-firacode-nerd \
     inter-font \
-    ttf-material-design-icons-webfont
+    ttf-material-design-icons-webfont \
+    ttf-sarasa-gothic
 
 install pipewire \
     pipewire wireplumber pipewire-audio pipewire-jack pipewire-alsa pipewire-pulse

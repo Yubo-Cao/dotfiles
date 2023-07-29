1b0953952b1a6bd631195aa32af50bfb6332b8a4
Yubo Cao <cao2006721@gmail.com>
2023-07-29 15:39:16 -0400

    fix(wayland): :bug: Don't use git version of the xdg-desktop-portal

--- private_dot_config/hypr/run_once_after_install.sh
+++ private_dot_config/hypr/run_once_after_install.sh
@@ -31,5 +31,5 @@ install "Hypr utilies" \
     xdg-utils gnome-keyring xorg-xwayland \
     autotiling-rs \
     polkit-dump-agent xorg-xhost \
-    xdg-desktop-portal-hyprland-git \
+    xdg-desktop-portal-hyprland \
     hyprpaper swayidle swaylock-effects-git

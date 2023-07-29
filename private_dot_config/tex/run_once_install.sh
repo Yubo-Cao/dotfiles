23147cc1a2d1e8d049e6f23aa8a347b45a1efe15
Yubo Cao <cao2006721@gmail.com>
2023-07-29 15:46:41 -0400

    fix(tex): :bug: Fix the auto-installer script to install LaTeX

--- private_dot_config/tex/run_once_install.sh
+++ private_dot_config/tex/run_once_install.sh
@@ -2,31 +2,43 @@
 source "$CHEZMOI_SOURCE_DIR/common.sh"
 
 install_texlive() {
-    if command -v tlmgr > /dev/null; then
+    if command -v tlmgr >/dev/null; then
         info "TeX Live is already installed"
         return
     fi
 
     info "Installing TeX Live"
-    tmp=$(mktemp -d)
-    trap "rm -rf $tmp" EXIT
-    cd "$tmp"
-    curl "https://mirror.ctan.org/systems/texlive/tlnet/install-tl-unx.tar.gz" --output "install-tl-unx.tar.gz"
-    zcat < "install-tl-unx.tar.gz" | tar -xvf -
-    cd install-tl-*
+    TEMP_DIRECTORY=$(mktemp -d)
+
+    # ensure that the temporary directory is removed on exit
+    trap 'rm -rf $TEMP_DIRECTORY' EXIT
+    trap 'rm -rf $TEMP_DIRECTORY' SIGINT
+
+    cd "$TEMP_DIRECTORY" || exit 1
+    wget "https://mirror.ctan.org/systems/texlive/tlnet/install-tl-unx.tar.gz" --output-document "install-tl-unx.tar.gz"
+    tar -xf install-tl-unx.tar.gz || exit 1
+
+    # try to cd to install-tl-*
+    for d in install-tl-*; do
+        if [ -d "$d" ]; then
+            cd "$d" || exit 1
+            break
+        fi
+    done
+
     info "Running TeX Live installer"
-    if pacman -Qq perl > /dev/null; then
+    if pacman -Qq perl >/dev/null; then
         info "perl is already installed"
     else
         info "Installing perl"
-        sudo pacman -S --noconfirm perl > /dev/null
+        sudo pacman -S --noconfirm perl >/dev/null
     fi
     sudo perl install-tl --paper letter \
         --no-src-install \
         --no-doc-install \
-        --no-interaction > /dev/null
-    info "TeX Live installed" 
-    rm -rf "$tmp"
+        --no-interaction || exit 1
+    info "TeX Live installed"
+    rm -rf "$TEMP_DIRECTORY"
     # environment variables are handled by Fish
 }
 

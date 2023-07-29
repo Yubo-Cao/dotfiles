caa07b8a011513bf1d9a6dfc6d2695bed8bd2737
Yubo-Cao <cao2006721@gmail.com>
2023-07-29 09:12:05 -0400

    fix: :fire: Remove unnecessary i3 configurations

--- private_dot_config/i3/script/exit.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-i3-msg mode exit
-i3-nagbar -t warning \
-    -m "Are you sure you want to exit i3?" \
-    -B "Yes, exit i3" "i3-msg exit" \
-    -B "Cancel" "i3-msg mode default && killall i3-nagbar"

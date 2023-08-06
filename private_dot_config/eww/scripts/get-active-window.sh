eae87554b2f8769da86754715b8f0d8abed637eb
Yubo Cao <cao2006721@gmail.com>
2023-08-06 14:32:51 -0400

    fix(eww): :sparkles: Add system tray to the eww

--- private_dot_config/eww/scripts/get-active-window.sh
+++ private_dot_config/eww/scripts/get-active-window.sh
@@ -2,4 +2,6 @@
 
 hyprctl activewindow -j | jq -Mcr ".title"
 socat -u "UNIX-CONNECT:/tmp/hypr/$HYPRLAND_INSTANCE_SIGNATURE/.socket2.sock" - |
-    stdbuf -o0 awk -F '>>|,' -e '/^activewindow>>/ {print $3}' -e '/^focusedmon>>/ {print $3}'
+    stdbuf -o0 awk -F '>>|,' \
+        -e '/^activewindow>>/ {print substr($3, 1, 79)}' \
+        -e '/^focusedmon>>/ {print substr($3, 1, 79)}'

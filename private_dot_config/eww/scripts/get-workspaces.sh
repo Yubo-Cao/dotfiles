a36cda125193d5c0865f4632f073cecdad925c69
Yubo Cao <cao2006721@gmail.com>
2023-08-06 14:02:06 -0400

    feat(eww): The first partial implementation of the eww bar

--- private_dot_config/eww/scripts/get-workspaces.sh
+++ private_dot_config/eww/scripts/get-workspaces.sh
@@ -2,5 +2,5 @@
 
 hyprctl workspaces -j | jq -Mc '[.[] | .name]'
 socat -u "UNIX-CONNECT:/tmp/hypr/$HYPRLAND_INSTANCE_SIGNATURE/.socket2.sock" - | while read -r line; do
-    hyprctl workspaces -j | jq -Mc '[.[] | .name]'
+    hyprctl workspaces -j | jq -Mc '[.[] | .name] | sort'
 done
